fa5dfe9a1eb49b3ae859df1001f642302a0fc19e
suresh kumar u <suresh@dteagritech.com>
2026-01-08 00:19:49 +0530

    config updated

--- watchList/src/index.ts
+++ watchList/src/index.ts
@@ -2,11 +2,13 @@ import express from 'express';
 import { AppDataSource } from './config/db.config';
 const app = express()
 
+
 AppDataSource.initialize().then(() => {
     console.log("Data Source has been initialized!");
 }).catch((err) => {
     console.error("Error during Data Source initialization:", err);
 });
+
 
 app.listen("8080", () => {
     console.log("Server is running on port 8080");
