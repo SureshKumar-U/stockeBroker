3d17550749e5e99bfcb07d30b4cebeb274d55521
suresh kumar u <suresh@dteagritech.com>
2026-01-15 16:20:15 +0530

    errorHandler and notFound middleware added

--- watchList/src/index.ts
+++ watchList/src/index.ts
@@ -1,6 +1,7 @@
 import express from 'express';
 import { AppDataSource } from './config/db.config';
 import router from './routes';
+import { errorHandler, notFound } from './middleware/errorHanlder';
 const app = express()
 
 
@@ -12,7 +13,8 @@ AppDataSource.initialize().then(() => {
 app.use(express.json());
 
 app.use("/api", router)
-
+app.use(notFound)
+app.use(errorHandler)
 
 app.listen("8080", () => {
     console.log("Server is running on port 8080");
