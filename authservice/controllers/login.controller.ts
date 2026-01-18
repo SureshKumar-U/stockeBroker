9324c50f01cf2bde2e376d6b25774d0a88aeca30
suresh kumar u <suresh@dteagritech.com>
2026-01-18 18:47:17 +0530

    accesstoken endpoint integrated

--- authservice/controllers/login.controller.ts
+++ authservice/controllers/login.controller.ts
@@ -57,3 +57,23 @@ export const callbackController = async (req: Request, res: Response) => {
 
 
 }
+
+export const getAccessToken = async (req: Request, res: Response) => {
+    try {
+        const email = req.query.email
+        if (!email) return res.status(400).json({ message: "email not found in params" })
+        const token = await accessTokenModel.findOne({ email: email })
+        if (!token) {
+            return res.status(200).json({ message: "accessToken not found with these email", email })
+        }
+        return res.status(200).json({ access_token: token?.accesstoken })
+
+    } catch (err) {
+        return res.status(500).json({
+            message: "Internal or External server Error",
+            error: err
+        })
+    }
+
+
+}
