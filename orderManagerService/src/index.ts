d880364e6ebb7804856ad053b70b8238dd5b00e6
suresh kumar u <suresh@dteagritech.com>
2026-01-19 17:54:54 +0530

    gerOrderDetails endpoint added

--- orderManagerService/src/index.ts
+++ orderManagerService/src/index.ts
@@ -1,7 +1,11 @@
 import express from "express"
 import dotenv from "dotenv"
 dotenv.config()
+import router from "./Routes"
 const app = express()
+
+
+app.use("/api/orders/",router)
 
 app.listen(process.env.PORT || 8001,()=>{
     console.log("server started on ", process.env.PORT)
