d03585fe24d95d04f2851bbc162ef78372db169b
suresh kumar u <suresh@dteagritech.com>
2026-01-25 23:26:26 +0530

    placeorder,cancelOrder,getOrders endpoint integrated

--- orderManagerService/src/controllers/index.ts
+++ orderManagerService/src/controllers/index.ts
@@ -1,18 +1,53 @@
 import { Request, Response } from "express";
 import { orderServices } from "../services/order.services";
 
-export  const OrderController = {
-    placeOrder:(req:Request,res:Response)=>{
+export const OrderController = {
+    placeOrder: async (req: Request, res: Response) => {
+
+        try {
+            const data: any = await orderServices.placeOrder(req.body)
+            if (data.status != 200) return res.status(data.status).json({ message: data.message, error: data.error })
+            return res.status(200).json({
+                message: "Order placed successfully",
+                data: data
+            })
+        } catch (err) {
+            console.log(err)
+            return res.status(500).json({
+                message: "Internal or External error",
+                error: err
+            })
+        }
 
 
     },
-    cancelOrder:()=>{
+    cancelOrder: async (req: Request, res: Response) => {
+        try {
+            const resul:any = await orderServices.cancelOrder(req.query.order_id as string)
+            console.log("result",resul)
+            const { status, data } = resul
+            return res.status(status).json({ data })
+        } catch (err) {
+            console.log(String(err))
+            return res.status(500).json({
+                error: String(err)
+            })
+        }
 
     },
-    getOrderDetails:async(req:Request,response:Response)=>{
+    getOrderDetails: async (req: Request, response: Response) => {
         const email = req?.query.email as string
         const orderId = req.params.order_id as string
-       const res=  await orderServices.getOrderDetails(email,orderId)
-       return response.status(200).json({res:res})
+        const res = await orderServices.getOrderDetails(email, orderId)
+        return response.status(200).json({ res: res })
+    },
+    getOrders: async (req: Request, res: Response) => {
+        try{
+        const result= await orderServices.getOrders()
+        return res.json(result)
+        }catch(err){
+            return res.status(500).json(err)
+        }
+
     }
 }
--- orderManagerService/src/services/order.services.ts
+++ orderManagerService/src/services/order.services.ts
@@ -1,19 +1,60 @@
+import console from "node:console";
+import { PlaceOrder } from "../types/orders.types"
+// let UpstoxClient = require('upstox-js-sdk');
+import * as UpstoxClient from "upstox-js-sdk";
 export const orderServices = {
-    getOrderDetails: async (email: string, orderId: string) => {
-        const getOrderDetailsUpstoxURL = `https://api.upstox.com/v2/order/details?order_id=${orderId}`
+    getOrders: async () => {
         const accesstokenURL = `http://localhost:9000/api/accesstoken?email=kumarsk12072000@gmail.com`
-
-        let res :any= await fetch(accesstokenURL, {
+        let res: any = await fetch(accesstokenURL, {
             method: "GET",
             headers: {
                 "Content-Type": "application/json",
                 "Accept": "application/json",
             }
         })
-        const {access_token,message} =await res.json()
+        const { access_token, message } = await res.json()
+        if (!access_token) {
+            return { status: 401, data: message }
+        }
+        let defaultClient = UpstoxClient.ApiClient.instance;
+        var OAUTH2 = defaultClient.authentications['OAUTH2'];
+        OAUTH2.accessToken = access_token;
+        let apiInstance = new UpstoxClient.OrderApi();
+        let apiVersion = "2.0";
+        return await new Promise((resolve, reject) => {
+            apiInstance.getOrderBook(apiVersion, (error, data, response) => {
+                if (error) {
+                    resolve({
+                        status: 400,
+                        data: error?.response?.text || "Cancel order failed",
+                    });
+                } else {
+                    resolve({
+                        status: 200,
+                        data: data,
+                    });
+                }
+            });
 
-        if(!access_token){
-            return {status:401,data:message}
+        })
+
+
+    },
+    getOrderDetails: async (email: string, orderId: string) => {
+        const getOrderDetailsUpstoxURL = `https://api.upstox.com/v2/order/details?order_id=${orderId}`
+        const accesstokenURL = `http://localhost:9000/api/accesstoken?email=kumarsk12072000@gmail.com`
+
+        let res: any = await fetch(accesstokenURL, {
+            method: "GET",
+            headers: {
+                "Content-Type": "application/json",
+                "Accept": "application/json",
+            }
+        })
+        const { access_token, message } = await res.json()
+
+        if (!access_token) {
+            return { status: 401, data: message }
         }
 
         let response = await fetch(getOrderDetailsUpstoxURL, {
@@ -26,7 +67,86 @@ export const orderServices = {
         })
 
         response = await response.json()
-        return {status:200 ,data: response }
+        return { status: 200, data: response }
+    },
+    placeOrder: async (place_order: PlaceOrder) => {
+        const accesstokenURL = `http://localhost:9000/api/accesstoken?email=kumarsk12072000@gmail.com`
+        let res: any = await fetch(accesstokenURL, {
+            method: "GET",
+            headers: {
+                "Content-Type": "application/json",
+                "Accept": "application/json",
+            }
+        })
+        const { access_token, message } = await res.json()
+        if (!access_token) {
+            return { status: 401, data: message }
+        }
+        let defaultClient = UpstoxClient?.ApiClient?.instance;
+        var OAUTH2 = defaultClient?.authentications['OAUTH2'];
+        OAUTH2.accessToken = access_token as string;
+        let apiInstance = new UpstoxClient.OrderApiV3();
+        let body = new UpstoxClient.PlaceOrderV3Request(1,
+            UpstoxClient.PlaceOrderV3Request.ProductEnum.D,
+            UpstoxClient.PlaceOrderV3Request.ValidityEnum.DAY,
+            0, "NSE_EQ|INE669E01016",
+            UpstoxClient.PlaceOrderV3Request.OrderTypeEnum.MARKET,
+            UpstoxClient.PlaceOrderV3Request.TransactionTypeEnum.BUY,
+            0, 0, true);
+        let opt = { "slice": true }
+        apiInstance.placeOrder(body, opt, (error, data, response) => {
+            if (error) {
+                console.error(error?.response?.text);
+                return { status: (error?.response?.status || error?.status || 400), message: "placeorder failed", error: error.response.text }
+
+            } else {
+                console.log('API called successfully. Returned data: ' + data);
+                return { status: 200, data: data }
+            }
+
+        });
+
+
+    },
+    cancelOrder: async (orderId: string) => {
+        //cancel order
+        const accesstokenURL = `http://localhost:9000/api/accesstoken?email=kumarsk12072000@gmail.com`
+        let res: any = await fetch(accesstokenURL, {
+            method: "GET",
+            headers: {
+                "Content-Type": "application/json",
+                "Accept": "application/json",
+            }
+        })
+        const { access_token, message } = await res.json()
+        if (!access_token) {
+            return { status: 401, data: message }
+        }
+        let defaultClient = UpstoxClient.ApiClient.instance;
+        var OAUTH2 = defaultClient.authentications['OAUTH2'];
+        OAUTH2.accessToken = access_token;
+        let apiInstance = new UpstoxClient.OrderApiV3();
+        return await new Promise((resolve, reject) => {
+            apiInstance.cancelOrder(orderId, (error, data, response) => {
+                if (error) {
+                    resolve({
+                        status: 400,
+                        data: error?.response?.text || "Cancel order failed",
+                    });
+                }
+                else {
+                    resolve({
+                        status: 200,
+                        data: data,
+                    });
+                }
+
+            });
+
+        })
+
+
+
     }
 
 }
